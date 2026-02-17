07ba74d5dd69cc8a0d4d03ef4926fd29db1e0f24
Ramzin007 <muhammedramzinp9@gmail.com>
2026-02-17 23:30:20 +0530

    phase 4  complete

--- index.js
+++ index.js
@@ -11,6 +11,49 @@ app.get("/", (req,res) => {
     res.render("index");
 })
 
+app.get("/search", async (req, res) => {
+  try {
+    const name = req.query.name;
+
+    if (!name) {
+      return res.redirect("/");
+    }
+
+    const response = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`
+    );
+
+    const drinks = response.data.drinks;
+
+    if (!drinks) {
+      return res.render("result", { drink: null });
+    }
+
+    res.render("result", { drink: drinks[0] });
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/random", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    );
+
+    const drink = response.data.drinks[0];
+
+    res.render("result", { drink });
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
 app.listen(port, () => {
     console.log(`This server is running on port ${port}`);
 })
